0292e4e8a388565ffbb76fd484790e9543d3a0f6
Pasqualino Titto Assini <tittoassini@gmail.com>
2017-04-12 17:31:08 +0200

    final fixes

--- src/Data/Flat.hs
+++ src/Data/Flat.hs
@@ -1,5 +1,5 @@
 module Data.Flat (
-    -- |Check the <https://github.com/tittoassini/fast tutorial and github repo>
+    -- |Check the <https://github.com/tittoassini/flat tutorial and github repo>.
     module X,
     UTF8Text(..),
     UTF16Text(..),
@@ -7,11 +7,10 @@ module Data.Flat (
     Decoded,
     DecodeException,
     ) where
--- import           Data.Flat.Bits                 as X
+
 import           Data.Flat.Class     as X
 import           Data.Flat.Decoder
 import           Data.Flat.Filler    as X
 import           Data.Flat.Instances as X
 import           Data.Flat.Run       as X
 import           Data.Flat.Types
--- import           Data.Flat.Pretty                  as X
