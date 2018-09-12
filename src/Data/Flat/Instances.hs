69f82e18a8f31baf446aebb5687574405c52a761
Pasqualino Titto Assini <tittoassini@gmail.com>
2018-09-12 15:38:34 +0200

    clarified a comment, formatted code

--- src/Data/Flat/Instances.hs
+++ src/Data/Flat/Instances.hs
@@ -9,33 +9,38 @@
 -- {-# LANGUAGE IncoherentInstances    #-}
 
 -- |Flat Instances for common, primitive and abstract data types for which instances cannot be automatically derived
-module Data.Flat.Instances (
-    sizeMap,
-    encodeMap,
-    decodeMap,
-    sizeSequence,
-    encodeSequence,
-    decodeSequence,
-    ) where
+module Data.Flat.Instances
+  ( sizeMap
+  , encodeMap
+  , decodeMap
+  , sizeSequence
+  , encodeSequence
+  , decodeSequence
+  )
+where
 
-import qualified Data.ByteString       as B
-import qualified Data.ByteString.Lazy  as L
-import qualified Data.ByteString.Short as SBS
+import qualified Data.ByteString               as B
+import qualified Data.ByteString.Lazy          as L
+import qualified Data.ByteString.Short         as SBS
 import           Data.Char
-import           Data.Containers       (ContainerKey, IsMap, MapValue,
-                                        mapFromList, mapToList)
+import           Data.Containers                ( ContainerKey
+                                                , IsMap
+                                                , MapValue
+                                                , mapFromList
+                                                , mapToList
+                                                )
 import           Data.Flat.Class
 import           Data.Flat.Decoder
 import           Data.Flat.Encoder
 --import           Data.Flat.Size        (arrayBits)
 import           Data.Flat.Types
-import qualified Data.Foldable         as F
-import qualified Data.Map              as M
+import qualified Data.Foldable                 as F
+import qualified Data.Map                      as M
 import           Data.MonoTraversable
-import qualified Data.Sequence         as S
+import qualified Data.Sequence                 as S
 import           Data.Sequences
-import qualified Data.Text             as T
-import           Prelude               hiding (mempty)
+import qualified Data.Text                     as T
+import           Prelude                 hiding ( mempty )
 
 -- Flat instances for common types
 instance Flat () where
@@ -58,11 +63,12 @@ instance {-# OVERLAPPABLE #-} (Flat a, Flat b, Flat c, Flat d, Flat e) => Flat (
 instance {-# OVERLAPPABLE #-} (Flat a, Flat b, Flat c, Flat d, Flat e, Flat f) => Flat (a,b,c,d,e,f)
 instance {-# OVERLAPPABLE #-} (Flat a, Flat b, Flat c, Flat d, Flat e, Flat f, Flat g) => Flat (a,b,c,d,e,f,g)
 
--- Do not provide this to 'force' users to declare instances of concrete list types
+-- Generic list instance.
 instance {-# OVERLAPPABLE #-} Flat a => Flat [a]
-instance {-# OVERLAPPING #-} Flat [Char]
 
--- instance Flat [Char]
+-- For better encoding/decoding performance, it is useful to declare instances of concrete list types.
+-- As this one for example:
+instance {-# OVERLAPPING #-} Flat [Char]
 
 -- Flat instances for primitive/abstract types
 instance Flat B.ByteString where
@@ -183,12 +189,18 @@ instance Flat a => Flat (S.Seq a) where
 -- |Calculate size of an instance of IsMap
 {-# INLINE sizeMap #-}
 sizeMap :: (Flat (ContainerKey r), Flat (MapValue r), IsMap r) => Size r
-sizeMap m acc = F.foldl' (\acc' (k,v) -> size k (size v (acc' + 1))) (acc+1) . mapToList $ m
+sizeMap m acc =
+  F.foldl' (\acc' (k, v) -> size k (size v (acc' + 1))) (acc + 1)
+    . mapToList
+    $ m
 
 {-# INLINE encodeMap #-}
 -- |Encode an instance of IsMap, as a list
-encodeMap :: (Flat (ContainerKey map), Flat (MapValue map), IsMap map) => map -> Encoding
-encodeMap = encodeListWith (\(k,v) -> encode k <> encode v) . mapToList
+encodeMap
+  :: (Flat (ContainerKey map), Flat (MapValue map), IsMap map)
+  => map
+  -> Encoding
+encodeMap = encodeListWith (\(k, v) -> encode k <> encode v) . mapToList
 -- encodeMap = go . mapToList
 --   where
 --     go []     = eFalse
@@ -196,12 +208,14 @@ encodeMap = encodeListWith (\(k,v) -> encode k <> encode v) . mapToList
 
 {-# INLINE decodeMap #-}
 -- |Decode an instance of IsMap, as a list
-decodeMap :: (Flat (ContainerKey map), Flat (MapValue map), IsMap map) => Get map
+decodeMap
+  :: (Flat (ContainerKey map), Flat (MapValue map), IsMap map) => Get map
 decodeMap = mapFromList <$> decodeListWith ((,) <$> decode <*> decode)
 
 {-# INLINE sizeSequence #-}
 -- |Calculate size of an instance of IsSequence
-sizeSequence :: (IsSequence mono, Flat (Element mono)) => mono -> NumBits -> NumBits
+sizeSequence
+  :: (IsSequence mono, Flat (Element mono)) => mono -> NumBits -> NumBits
 sizeSequence s acc = ofoldl' (flip size) acc s + arrayBits (olength s)
 
 {-# INLINE encodeSequence #-}
