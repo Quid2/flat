6abc3d1b999f2258f131bcd224c8d420fae34ca8
Pasqualino Titto Assini <tittoassini@gmail.com>
2018-11-08 15:58:07 +0100

    various fixes for eta

--- benchmarks/Mini.hs
+++ benchmarks/Mini.hs
@@ -3,6 +3,7 @@
 {-# LANGUAGE DeriveGeneric       #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
+module Main where
 import           Control.Concurrent
 import           Control.DeepSeq
 import           Criterion.IO
@@ -10,6 +11,7 @@ import           Criterion.Main
 import           Criterion.Types
 import qualified Data.ByteString    as B
 import           Data.Flat
+import qualified Data.Map           as M
 import           Report
 import           System.Directory
 import           System.FilePath
@@ -52,8 +54,8 @@ setupEnv = do
   let v3 = v2
   -- let v3 = (E256_256, B.pack [255, 1])
   let v4 = valTest treeLargeT
-  print $ "Size Tree is " ++ show (getSize treeLarge)
-  print $ "Size Tree " ++ show (5999999 == getSize treeLarge)
+  -- print $ "Size Tree is " ++ show (getSize treeLarge)
+  -- print $ "Size Tree " ++ show (5999999 == getSize treeLarge)
   -- print $ "Size Tree " ++ show (6999999 == getSize treeLarge)
   -- print $ unwords ["Size S3 ",show ( == getSize S1treeLarge)
   -- print v4
@@ -69,18 +71,41 @@ workDir = projDir </> "benchmarks/data"
 
 tmpDir = "/tmp"
 
+data REC = REC {l::Int,r::Int} deriving (Read,Show)
+
 main
  = do
+  -- print "OK"
+  -- -- print (read (show (M.fromList []:: M.Map Int Int)) :: M.Map Int Int)
+  -- print (read "106.31866268954991" :: Double)
+  -- print (read "\"runtime-basic/dec\"" :: String)
+  -- print (read "False" :: Bool)
+  -- print (read "(False,3234.34)" :: (Bool,Double))
+  -- -- print (read "TRUE \"ab\" 3.4545" :: BOOL)
+  -- print (read "4" :: Int)
+  -- print (read "REC {l=3,r=4}" :: REC)
+  -- print (read "REC 3 4" :: REC)
+  -- -- print (read "FALSE 3.45" :: BOOL)
+  -- print (read "Right False" :: Either () Bool)
+  -- print (read "Measure \"runtime-basic/dec\" 106.31866268954991" :: Measure)
+  -- print (read "Measure {mTest = \"runtime-basic/dec\", mValue = 106.31866268954991}" :: Measure)
   createDirectoryIfMissing True workDir
   mainBench_ (reportsFile workDir)
-  -- deleteMeasures workDir
-  ms <- updateMeasures_ workDir
-  printMeasuresDiff ms
+  -- printMeasures
+
+printMeasures = do  
+    -- delete measures to avoid eta read bug
+    -- deleteMeasures workDir
+    ms <- updateMeasures_ workDir
+    printMeasuresDiff ms
     -- printMeasuresAll ms
-  printMeasuresCurrent ms
+    printMeasuresCurrent ms
 
 mainBench_ jsonReportFile =
-  defaultMainWith (defaultConfig {jsonFile = Just jsonReportFile}) [runtime]
+  defaultMainWith (defaultConfig {
+   jsonFile = Just jsonReportFile
+   -- ,verbosity=Quiet -- avoid trouble with unit of measure character
+    }) [runtime]
   where
     runtime =
       env setupEnv $ \ ~(v1, v2, v3, v4 ) ->
@@ -95,13 +120,13 @@ mainBench_ jsonReportFile =
            ,bench "dec eq" $ whnf (\tv -> val tv == val tv) v4
            ,bench "dec" $ whnf (\tv -> decod (enc tv) == Right (val tv)) v4
           ]
-    compilation = bgroup "compilation-basic" [bench "compile" $ nfIO comp]
+    --compilation = bgroup "compilation-basic" [bench "compile" $ nfIO comp]
 
-comp = do
-  callCommand $ concat ["touch ", projDir </> "test/Test/E/Flat.hs"]
-  callCommand $
-    concat
-      ["cd ", projDir, ";stack ghc -- -isrc  -itest -O test/Test/E/Flat.hs"]
+-- comp = do
+--   callCommand $ concat ["touch ", projDir </> "test/Test/E/Flat.hs"]
+--   callCommand $
+--     concat
+--       ["cd ", projDir, ";stack ghc -- -isrc  -itest -O test/Test/E/Flat.hs"]
 
 fromRight (Right a) = a
 -- fromRight (Left e) = error $ show e
