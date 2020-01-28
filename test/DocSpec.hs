16227de1cc68e31889bcb9dfda9814c73fd7846f
Pasqualino Titto Assini <tittoassini@gmail.com>
2020-01-28 16:55:50 +0100

    support for specifying files to process

--- test/DocSpec.hs
+++ test/DocSpec.hs
@@ -1,17 +1,27 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
 import           Data.List                      ( isSuffixOf )
 import           System.FilePath.Find
 import           Test.DocTest
+import           System.Environment
+import qualified Data.Text                     as T
 
 main :: IO ()
 main = do
-  files <- find always ((extension ==? ".hs") &&? exceptFiles []) "src"
-  -- print files   
+  args <- getArgs
+  print args
+  files <- if length args > 0
+    then return $ map
+      (T.unpack . (<> ".hs") . ("src/" <>) . T.replace "." "/" . T.pack)
+      args
+    else find always ((extension ==? ".hs") &&? exceptFiles []) "src"
+  print files
   doctest $ "-XCPP" : files
 
 exceptFiles :: Foldable t => t String -> FindClause Bool
 exceptFiles mdls =
   let excludes = liftOp (\fp modules -> not $ any (`isSuffixOf` fp) modules)
   in  filePath `excludes` mdls
-      -- let excludes = liftOp (\fp mdls -> not $ any (\mdl -> isSuffixOf mdl (traceShowId fp)) mdls)
+-- let excludes = liftOp (\fp mdls -> not $ any (\mdl -> isSuffixOf mdl (traceShowId fp)) mdls)
