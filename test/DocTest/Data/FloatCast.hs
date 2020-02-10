36541ff06e2195a7bc10f7164e22d36a3e8dac1e
Pasqualino Titto Assini <tittoassini@gmail.com>
2020-02-10 15:29:07 +0100

    eta fix

--- test/DocTest/Data/FloatCast.hs
+++ test/DocTest/Data/FloatCast.hs
@@ -4,6 +4,7 @@ module DocTest.Data.FloatCast where
 import qualified DocTest
 import Test.Tasty(TestTree,testGroup)
 import Data.FloatCast
+import Data.Word
 
 tests :: IO TestTree
-tests = testGroup "Data.FloatCast" <$> sequence [  DocTest.testProp "src/Data/FloatCast.hs:38" ( \f -> wordToFloat (floatToWord f ) == f ),  DocTest.test "src/Data/FloatCast.hs:40" ["3189768192"] (DocTest.asPrint( floatToWord (-0.15625) )),  DocTest.test "src/Data/FloatCast.hs:43" ["-0.15625"] (DocTest.asPrint( wordToFloat 3189768192 )),  DocTest.testProp "src/Data/FloatCast.hs:58" ( \f -> wordToDouble (doubleToWord f ) == f ),  DocTest.test "src/Data/FloatCast.hs:60" ["13818169556679524352"] (DocTest.asPrint( doubleToWord (-0.15625) )),  DocTest.test "src/Data/FloatCast.hs:63" ["-0.15625"] (DocTest.asPrint( wordToDouble 13818169556679524352 ))]
+tests = testGroup "Data.FloatCast" <$> sequence [  DocTest.testProp "src/Data/FloatCast.hs:39" ( \f -> wordToFloat (floatToWord f ) == f ),  DocTest.test "src/Data/FloatCast.hs:41" ["3189768192"] (DocTest.asPrint( floatToWord (-0.15625) )),  DocTest.test "src/Data/FloatCast.hs:44" ["-0.15625"] (DocTest.asPrint( wordToFloat 3189768192 )),  DocTest.testProp "src/Data/FloatCast.hs:59" ( \f -> wordToDouble (doubleToWord f ) == f ),  DocTest.test "src/Data/FloatCast.hs:61" ["13818169556679524352"] (DocTest.asPrint( doubleToWord (-0.15625) )),  DocTest.test "src/Data/FloatCast.hs:64" ["-0.15625"] (DocTest.asPrint( wordToDouble 13818169556679524352 )),  DocTest.test "src/Data/FloatCast.hs:88" ["True"] (DocTest.asPrint( runST (cast (0xF0F1F2F3F4F5F6F7::Word64)) == (0xF0F1F2F3F4F5F6F7::Word64) ))]
