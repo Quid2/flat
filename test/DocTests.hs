562d58e10bd437617a1000b017c1accd8aa7a0e6
Pasqualino Titto Assini <tittoassini@gmail.com>
2020-02-10 15:03:39 +0100

    eta fix

--- test/DocTests.hs
+++ test/DocTests.hs
@@ -12,5 +12,6 @@ import qualified DocTest.Data.Flat.Instances.Text
 import qualified DocTest.Data.ZigZag
 import qualified DocTest.Data.Flat.Decoder.Prim
 import qualified DocTest.Data.FloatCast
+import qualified DocTest.Data.Flat.Endian
 
-main = (testGroup "DocTests" <$> sequence [DocTest.Data.Flat.Instances.ByteString.tests,DocTest.Data.Flat.Instances.DList.tests,DocTest.Data.Flat.Instances.Containers.tests,DocTest.Data.Flat.Instances.Base.tests,DocTest.Data.Flat.Instances.Unordered.tests,DocTest.Data.Flat.Instances.Vector.tests,DocTest.Data.Flat.Instances.Mono.tests,DocTest.Data.Flat.Instances.Text.tests,DocTest.Data.ZigZag.tests,DocTest.Data.Flat.Decoder.Prim.tests,DocTest.Data.FloatCast.tests]) >>= defaultMain
+main = (testGroup "DocTests" <$> sequence [DocTest.Data.Flat.Instances.ByteString.tests,DocTest.Data.Flat.Instances.DList.tests,DocTest.Data.Flat.Instances.Containers.tests,DocTest.Data.Flat.Instances.Base.tests,DocTest.Data.Flat.Instances.Unordered.tests,DocTest.Data.Flat.Instances.Vector.tests,DocTest.Data.Flat.Instances.Mono.tests,DocTest.Data.Flat.Instances.Text.tests,DocTest.Data.ZigZag.tests,DocTest.Data.Flat.Decoder.Prim.tests,DocTest.Data.FloatCast.tests,DocTest.Data.Flat.Endian.tests]) >>= defaultMain
