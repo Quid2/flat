2f82a9b7d29b221f5f5822eafb5920382d9b1e81
Pasqualino Titto Assini <tittoassini@gmail.com>
2020-05-14 20:18:01 +0200

    removed Get from exports

--- src/Flat.hs
+++ src/Flat.hs
@@ -1,19 +1,26 @@
-module Flat (
+{-|
+Haskell implementation of <http://quid2.org/docs/Flat.pdf Flat>, a principled, portable and efficient binary data format (<http://quid2.org specs>).
+
+-}
+module Flat
+  (
     -- |Check the <https://github.com/Quid2/flat tutorial and github repo>.
-    module Flat.Class,
-    module Flat.Filler,
-    module X, -- module Flat.Instances,
+    module Flat.Class
+  , module Flat.Filler
+  , module X
+  , -- module Flat.Instances,
     -- module Flat.Run,
     -- UTF8Text(..),
     -- UTF16Text(..),
-    Get,
-    Decoded,
-    DecodeException,
-    ) where
+    -- Get,
+    Decoded
+  , DecodeException
+  )
+where
 
 import           Flat.Class
 import           Flat.Decoder
 import           Flat.Filler
-import           Flat.Instances as X
-import           Flat.Run as X
-import           Flat.Types()
+import           Flat.Instances                as X
+import           Flat.Run                      as X
+import           Flat.Types                     ( )
