1642180b26e42598f347862f0d2253791967e7a8
Pasqualino Titto Assini <tittoassini@gmail.com>
2021-02-03 15:35:32 +0100

    added Flat.Repr

---
+++ src/Flat/Repr.hs
@@ -0,0 +1,22 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+module Flat.Repr where
+
+import Flat.Class ( Flat(..) )
+import qualified Data.ByteString as B
+import Flat.Decoder.Types ( Get )
+import Flat.Run ( flat, unflat )
+
+-- Flat representation of a value
+newtype Repr a = Repr {repr :: B.ByteString}
+
+-- Get the underlying value
+unrepr :: Flat a => Repr a -> a
+unrepr (Repr bs)= let Right a = unflat bs in a
+
+instance Flat a => Flat (Repr a) where
+    size = error "unused"
+    encode = error "unused"
+
+    -- To create the representation we just re 'flat' the parsed value (this could be optimised by copying directly the parsed representation)
+    decode = Repr . flat <$> (decode :: Get a)
+
