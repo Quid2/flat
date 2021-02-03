ebb366eb09f2cd14a28cd7dcf1055871f2b78829
Pasqualino Titto Assini <tittoassini@gmail.com>
2021-02-03 15:30:15 +0100

    added decodeListTWith

--- src/Flat/Decoder.hs
+++ src/Flat/Decoder.hs
@@ -2,6 +2,7 @@
 -- |Strict Decoder
 module Flat.Decoder (
     strictDecoder,
+    decodeListTWith,
     -- strictDecoderPart,
     Decoded,
     DecodeException(..),
@@ -48,3 +49,22 @@ module Flat.Decoder (
 import           Flat.Decoder.Prim
 import           Flat.Decoder.Strict
 import           Flat.Decoder.Types
+import           Foreign
+
+import ListT
+import qualified Data.ByteString.Internal as BS
+
+decodeListTWith :: Get a -> BS.ByteString -> IO (ListT IO a)
+decodeListTWith get (BS.PS base off len) = 
+    withForeignPtr base $ \base0 -> do
+        let ptr = base0 `plusPtr` off
+            endPtr = ptr `plusPtr` len
+            s = S ptr 0
+            go s = do
+                GetResult s' b <- runGet dBool endPtr s
+                if b
+                    then do
+                        GetResult s'' a <- runGet get endPtr s'
+                        return $ Just (a, ListT $ go s'')
+                    else return Nothing
+        return $ ListT (go s)
