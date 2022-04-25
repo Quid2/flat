88defd9f97a4819ffdd40d419989d260dcc6e00b
Pasqualino Titto Assini <tittoassini@gmail.com>
2022-04-25 12:50:25 +0200

    additional tests

--- test/DocTest/Flat/Instances/Extra.hs
+++ test/DocTest/Flat/Instances/Extra.hs
@@ -4,6 +4,7 @@ module DocTest.Flat.Instances.Extra where
 import qualified DocTest
 import Test.Tasty(TestTree,testGroup)
 import Flat.Instances.Extra
+import Flat.Instances.Test
 
 tests :: IO TestTree
-tests = testGroup "Flat.Instances.Extra" <$> sequence [  DocTest.test "src/Flat/Instances/Extra.hs:10" "[ExpectedLine [LineChunk \"(True,1,\\\"0\\\")\"]]" (DocTest.asPrint( test "" )),  DocTest.test "src/Flat/Instances/Extra.hs:13" "[ExpectedLine [LineChunk \"(True,28,\\\"10110000 11011000 01101100 0010\\\")\"]]" (DocTest.asPrint( test "aaa" ))]
+tests = testGroup "Flat.Instances.Extra" <$> sequence [  DocTest.test "src/Flat/Instances/Extra.hs:13" "[ExpectedLine [LineChunk \"(True,1,\\\"0\\\")\"]]" (DocTest.asPrint( tstBits "" )),  DocTest.test "src/Flat/Instances/Extra.hs:16" "[ExpectedLine [LineChunk \"(True,28,\\\"10110000 11011000 01101100 0010\\\")\"]]" (DocTest.asPrint( tstBits "aaa" ))]
