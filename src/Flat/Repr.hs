589e778dc5c789c1cbe57b6ee1a9b4108bb4ddc8
Pasqualino Titto Assini <tittoassini@gmail.com>
2022-10-03 16:31:23 +0200

    added docs

--- src/Flat/Repr.hs
+++ src/Flat/Repr.hs
@@ -1,19 +1,44 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 module Flat.Repr where
 
-import Flat.Class ( Flat(..) )
-import qualified Data.ByteString as B
-import Flat.Decoder.Types ( Get )
-import Flat.Run ( flat, unflat )
+import qualified Data.ByteString    as B
+import           Flat.Class         (Flat (..))
+import           Flat.Decoder.Types (Get)
+import           Flat.Run           (flat, unflat)
 
--- Flat representation of a value
-newtype Repr a = Repr {repr :: B.ByteString}
+-- $setup
+-- >>> :set -XScopedTypeVariables
+-- >>> import Flat.Instances.Base
+-- >>> import Flat.Decoder.Types
+-- >>> import Flat.Types
+-- >>> import Flat.Run
+
+{- | Flat representation of a value
+
+It is occasionally useful to keep a decoded value, or part of it, in its encoded binary representation and decode it later on demand.
+
+To do so, just decode a value `a` to a `Repr a`.
+
+For example, we encode a list of Ints and then decode it to a list of Repr Int:
+
+>>> unflat (flat [1::Int .. 5]) :: Decoded ([Repr Int])
+Right [Repr {repr = "\STX\SOH"},Repr {repr = "\EOT\SOH"},Repr {repr = "\ACK\SOH"},Repr {repr = "\b\SOH"},Repr {repr = "\n\SOH"}]
+
+To decode a `Repr a` to an `a`, we use `unrepr`:
+
+>>> let Right l = unflat (flat [1..5]) :: Decoded [Repr Int] in unrepr (l  !! 2)
+3
+
+See test/FlatRepr.hs for a test and an example of use.
+-}
+
+newtype Repr a = Repr {repr :: B.ByteString} deriving Show
 
 -- Get the underlying value
 unrepr :: Flat a => Repr a -> a
-unrepr (Repr bs)= 
+unrepr (Repr bs)=
     case unflat bs of
-        Right a -> a 
+        Right a -> a
         Left _  -> error "impossible"
 
 instance Flat a => Flat (Repr a) where
@@ -22,4 +47,3 @@ instance Flat a => Flat (Repr a) where
 
     -- To create the representation we just re 'flat' the parsed value (this could be optimised by copying directly the parsed representation)
     decode = Repr . flat <$> (decode :: Get a)
-
